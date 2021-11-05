8b5751f17595edfd4af24fd8d1d4e1628ae79c1d
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-11-04 18:05:55 -0600

    basic table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 function App() {
+  const [movieData, setMovieData] = useState(null)
+
+  useEffect(()=>{
+    fetch('https://skyit-coding-challenge.herokuapp.com/movies')
+    .then(res=>{
+      return res.json();
+    })
+    .then(res=>{ setMovieData(res);})
+    .catch(err=>{console.log(err)})
+  }, [])
+  // const dynamicColumns = Object.keys(movieData[0]).filter(key=>{
+  //   if(key === '__v' || key === '_id'){
+  //     return false;
+  //   }else{
+  //     return true;
+  //   }
+  // }).map((col,i) => {
+  //   return <Column key={col} field={col} header={col} />;
+  // });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(movieData)}
+        <DataTable value={movieData} dataKey='_id' header='Favourite Movie List' rowHover selection selectionMode='radiobutton'>
+            <Column field={'title'} header={'Title'} body={(row)=>row.title}/>
+            <Column field={'releaseDate'} header={'Year'} />
+            <Column field={'length'} header={'Running Time'} />
+            <Column field={'director'} header={'Director'} />
+            <Column field={'certification'} header={'Certification'} />
+            <Column field={'rating'} header={'Rating'} />
+        </DataTable>
     </div>
   );
 }
