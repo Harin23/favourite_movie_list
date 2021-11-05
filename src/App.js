7dd79f8c5be2e3833f5a04915247db0b1d7f7708
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-11-05 12:18:35 -0600

    Other filters fixed, Director and Certifications filters set up completed

--- src/App.js
+++ src/App.js
@@ -1,89 +1,212 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import '../node_modules/primereact/resources/themes/saga-blue/theme.css'
-import '../node_modules/primereact/resources/primereact.min.css'
-import '../node_modules/primeicons/primeicons.css'
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { classNames } from 'primereact/utils';
-import { MultiSelect } from 'primereact/multiselect';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+// import "../node_modules/primereact/resources/themes/saga-blue/theme.css";
+// import "../node_modules/primereact/resources/primereact.min.css";
+// import "../node_modules/primeicons/primeicons.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.css";
+import "primeflex/primeflex.css";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { classNames } from "primereact/utils";
+import { MultiSelect } from "primereact/multiselect";
+import { Dropdown } from "primereact/dropdown";
 
 function App() {
   const [movieData, setMovieData] = useState(null);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [loadingTable, setLoadingTable] = useState(true);
   const [uniqueDirectors, setUniqueDirectors] = useState([]);
+  const [selectedDirectors, setSelectedDirectors] = useState(null);
+  const [selectedCertifications, setSelectedCertifications] = useState(null);
+  const myRef = useRef(null);
+  // const [tableFilters, setTableFilters] = useState({
+  //   'title': { value: null, matchMode: 'startsWith' },
+  //   'releaseDate': { value: null, matchMode: 'startsWith'},
+  //   'date': { value: null, matchMode: 'startsWith'},
+  //   'representative': { value: null, matchMode: 'in' },
+  //   'balance': { value: null, matchMode: 'equals' },
+  //   'status': { value: null, matchMode: 'startsWith'},
+  // })
 
-  useEffect(()=>{
-    fetch('https://skyit-coding-challenge.herokuapp.com/movies')
-    .then(res=>{
-      return res.json();
-    })
-    .then(res=>{ 
-      // console.log(res); 
-      setLoadingTable(false); 
-      setMovieData(res);
-      setUniqueDirectors(res.map(row=>row.director).filter((director, index, selfArr)=>selfArr.indexOf(director)===index).map(director=>{return {'name': director}}));
-    })
-    .catch(err=>{console.log(err)})
-  }, [])
+  useEffect(() => {
+    fetch("https://skyit-coding-challenge.herokuapp.com/movies")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        // console.log(res);
+        setLoadingTable(false);
+        setMovieData(
+          res.map((row) => {
+            row.rating = (Math.round((row.rating / 5) * 10000) / 100).toFixed(2) + "%";
+            return row;
+          })
+        );
+        setUniqueDirectors(
+          res
+            .map((row) => row.director)
+            .filter(
+              (director, index, selfArr) => selfArr.indexOf(director) === index
+            )
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  let certificationBodyTemplate=(rowData)=>{
+  let certificationBodyTemplate = (rowData) => {
     const certificationClassName = classNames({
-        'green-certification': rowData.certification === 'General',
-        'red-certification': rowData.certification === '14 Accompaniment',
-        'yellow-certification': rowData.certification === 'CA-PG'
+      "green-certification": rowData.certification === "General",
+      "red-certification": rowData.certification === "14 Accompaniment",
+      "yellow-certification": rowData.certification === "CA-PG",
     });
 
     return (
-        <span className={certificationClassName}>
-            {rowData.certification}
-        </span>
+      <span className={certificationClassName}>{rowData.certification}</span>
     );
-  }
+  };
 
-  let filterFunc=(val, filter)=>{
-    console.log(val, ((Math.round((val/5)*10000)/100).toFixed(2)+'%'), filter)
-    return ((Math.round((val/5)*10000)/100).toFixed(2)+'%').startsWith(filter)!==false ? true:false;
-  }
-
-  let directorsListTemplate=(option)=>{
+  let directorsListTemplate = (option) => {
     return (
-      <div className="p-multiselect-representative-option">
-        {option.name}
-      </div>
+      <div className="p-multiselect-representative-option">{option}</div>
     );
-}
+  };
 
-  let directorColFilterTemplate=(options)=>{
-    return <MultiSelect value={options.value} options={uniqueDirectors} itemTemplate={directorsListTemplate} onChange={(e) => options.filterApplyCallback(e.value)} optionLabel="name" placeholder="Any" className="p-column-filter" maxSelectedLabels={1} />;
-  }
-  
+  let directorColFilterTemplate = (options) => {
+    // console.log(options);
+    return (
+      <MultiSelect
+        value={selectedDirectors}
+        options={uniqueDirectors}
+        itemTemplate={directorsListTemplate}
+        onChange={(e) => {
+          myRef.current.filter(e.value, 'director', 'in');
+          setSelectedDirectors(e.value);
+        }}
+        placeholder="Any"
+        className="p-column-filter"
+      />
+    );
+  };
+
+  let certificationListTemplate = (option) => {
+    const certificationClassName = classNames({
+      "green-certification": option.name === "General",
+      "red-certification": option.name === "14 Accompaniment",
+      "yellow-certification": option.name === "CA-PG",
+    });
+    return (
+      <span
+        className={
+          "p-multiselect-representative-option " + certificationClassName
+        }
+      >
+        {option}
+      </span>
+    );
+  };
+
+  let certificationColFilterTemplate = (options) => {
+    return (
+      <Dropdown
+        value={selectedCertifications}
+        options={[
+         "General",
+          "CA-PG",
+          "14 Accompaniment"
+        ]}
+        itemTemplate={certificationListTemplate}
+        onChange={(e) => {
+          myRef.current.filter(e.value, 'certification', 'equals');
+          setSelectedCertifications(e.value);
+        }}
+        placeholder="Any"
+        className="p-column-filter"
+      />
+    );
+  };
+
   return (
     <div className="App">
-        <DataTable value={movieData}
-         dataKey='_id' 
-         header='Favourite Movie List'
-         rowHover
-         selection={selectedMovie}
-         selectionMode='radiobutton'
-         onSelectionChange={(e)=>setSelectedMovie(e.value)}
-         onRowSelect={()=>{console.log(selectedMovie)}}
-         paginator={true}
-         rows={10}
-         globalFilterFields={['title', 'releaseDate', 'length', 'rating']}
-         filterDisplay='row'
-         loading={loadingTable}
- 
-        >
-            <Column selectionMode="single" headerStyle={{width: '3em'}}/>
-            <Column field={'title'} header={'Title'} body={(row)=>row.title} filter filterPlaceholder='search by title' showClearButton={false} showFilterMenu={false}/>
-            <Column field={'releaseDate'} header={'Year'} body={(row)=>row.releaseDate} filter filterPlaceholder='search by year' showClearButton={false} showFilterMenu={false}/>
-            <Column field={'length'} header={'Running Time'} body={(row)=>row.length} filter filterPlaceholder='search by time' showClearButton={false} showFilterMenu={false}/>
-            <Column filterField={'director'} header={'Director'} body={(row)=>row.director} filter filterElement={directorColFilterTemplate} showClearButton={false} showFilterMenu={false} />
-            <Column field={'certification'} header={'Certification'} body={certificationBodyTemplate} filter showClearButton={false} showFilterMenu={false} />
-            <Column field={'rating'} header={'Rating'} body={(row)=>(Math.round((row.rating/5)*10000)/100).toFixed(2)+'%'} filter filterPlaceholder='search by rating' showClearButton={false} showFilterMenu={false} filterMatchMode="custom" filterFunction={filterFunc}/>
-        </DataTable>
+      <DataTable
+        ref={myRef}
+        value={movieData}
+        dataKey="_id"
+        header="Favourite Movie List"
+        rowHover
+        selection={selectedMovie}
+        selectionMode="radiobutton"
+        onSelectionChange={(e) => setSelectedMovie(e.value)}
+        // onRowSelect={() => {
+        //   console.log(selectedMovie);
+        // }}
+        paginator={true}
+        rows={10}
+        filterDisplay="row"
+        // filters={tableFilters}
+        loading={loadingTable}
+      >
+        <Column selectionMode="single" headerStyle={{ width: "3em" }} />
+        <Column
+          field={"title"}
+          header={"Title"}
+          body={(row) => row.title}
+          filter
+          filterPlaceholder="search by title"
+          showClearButton={false}
+          showFilterMenu={false}
+        />
+        <Column
+          field={"releaseDate"}
+          header={"Year"}
+          body={(row) => row.releaseDate}
+          filter
+          filterPlaceholder="search by year"
+          showClearButton={false}
+          showFilterMenu={false}
+        />
+        <Column
+          field={"length"}
+          header={"Running Time"}
+          body={(row) => row.length}
+          filter
+          filterPlaceholder="search by time"
+          showClearButton={false}
+          showFilterMenu={false}
+        />
+        <Column
+          header={"Director"}
+          filterField={"director"}
+          showFilterMenu={false}
+          filterMenuStyle={{ width: "14rem" }}
+          style={{ minWidth: "14rem" }}
+          body={(row) => <React.Fragment>{row.director}</React.Fragment>}
+          filter
+          filterElement={directorColFilterTemplate}
+        />
+        <Column
+          filterField={"certification"}
+          header={"Certification"}
+          body={certificationBodyTemplate}
+          filterMatchMode="equals"
+          filterMenuStyle={{ width: "14rem" }}
+          style={{ minWidth: "14rem" }}
+          filter
+          filterElement={certificationColFilterTemplate}
+          showFilterMenu={false}
+        />
+        <Column
+          field={"rating"}
+          header={"Rating"}
+          filter
+          filterPlaceholder="search by rating"
+          showClearButton={false}
+          showFilterMenu={false}
+        />
+      </DataTable>
     </div>
   );
 }
