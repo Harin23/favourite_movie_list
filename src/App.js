4a50cb1bf91165d2a78692d73354493009c368fd
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-11-04 19:30:14 -0600

    Added theme, radiobutton, pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import '../node_modules/primereact/resources/themes/saga-blue/theme.css'
+import '../node_modules/primereact/resources/primereact.min.css'
+import '../node_modules/primeicons/primeicons.css'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function App() {
-  const [movieData, setMovieData] = useState(null)
+  const [movieData, setMovieData] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(()=>{
     fetch('https://skyit-coding-challenge.herokuapp.com/movies')
@@ -14,25 +18,27 @@ function App() {
     .then(res=>{ setMovieData(res);})
     .catch(err=>{console.log(err)})
   }, [])
-  // const dynamicColumns = Object.keys(movieData[0]).filter(key=>{
-  //   if(key === '__v' || key === '_id'){
-  //     return false;
-  //   }else{
-  //     return true;
-  //   }
-  // }).map((col,i) => {
-  //   return <Column key={col} field={col} header={col} />;
-  // });
   return (
     <div className="App">
-      {console.log(movieData)}
-        <DataTable value={movieData} dataKey='_id' header='Favourite Movie List' rowHover selection selectionMode='radiobutton'>
-            <Column field={'title'} header={'Title'} body={(row)=>row.title}/>
+      {/* {console.log(movieData)} */}
+        <DataTable value={movieData}
+         dataKey='_id' 
+         header='Favourite Movie List'
+         rowHover
+         selection={selectedMovie}
+         selectionMode='radiobutton'
+         onSelectionChange={(e)=>setSelectedMovie(e.value)}
+         onRowSelect={()=>{console.log(selectedMovie)}}
+         paginator={true}
+         rows={10}
+        >
+            <Column selectionMode="single" headerStyle={{width: '3em'}} />
+            <Column field={'title'} header={'Title'}/>
             <Column field={'releaseDate'} header={'Year'} />
             <Column field={'length'} header={'Running Time'} />
             <Column field={'director'} header={'Director'} />
             <Column field={'certification'} header={'Certification'} />
-            <Column field={'rating'} header={'Rating'} />
+            <Column field={'rating'} header={'Rating'} body={(row)=>(Math.round((row.rating/5)*10000)/100).toFixed(2).toString()+'%'} />
         </DataTable>
     </div>
   );
